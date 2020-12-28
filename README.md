a41fc6a354839d53c99f7df33ac8d647a3dfb7bb
xieyh1336 <1336140321@qq.com>
2020-12-28 16:51:05 +0800

    no message

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # emotion
 键盘等高的表情面板构造者模式，TextView的Gif加载
+需要修改的地方：
+activity的主题：
+<item name="android:windowFullscreen">false</item>
+<item name="android:windowTranslucentStatus">true</item>
--- app/src/main/java/com/example/emotionkeyboardbuilder/builder/EmotionKeyboardBuilder.java
+++ app/src/main/java/com/example/emotionkeyboardbuilder/builder/EmotionKeyboardBuilder.java
@@ -18,7 +18,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
-import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
